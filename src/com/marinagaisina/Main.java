6f24468495800406da7a0e49b7e82b3fa4954774
Marina Gaisina <farigatovna@hotmail.com>
2022-01-03 09:55:01 -0500

    Initial commit

---
+++ src/com/marinagaisina/Main.java
@@ -0,0 +1,16 @@
+package com.marinagaisina;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static Boolean isValid(String userName) {
+        String[] invalidCharArray = "!@#$%^&*()".split("");
+        List<String> invalidChars = Arrays.asList(invalidCharArray);
+        String[] userNameCharacterArray
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/marinagaisina/MainApplictationTest.java
@@ -0,0 +1,4 @@
+package com.marinagaisina;
+
+public class MainApplictationTest {
+}
